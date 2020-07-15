10e213078ae4cb869941e893e1d3837945c34dd6
divyagb29 <divya2229mgb@gmail.com>
2020-07-15 18:01:02 +0530

    printing week days

---
+++ weekday.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+echo -n "enter the week number : "
+read num
+case $num in
+	1)
+	echo "sunday"
+	;;
+	2)
+	echo "monday"
+	;;
+	3)
+	echo "tuesday"
+	;;
+	4)
+	echo "wednesday"
+	;;
+	5)
+	echo "thursday"
+	;;
+	6)
+	echo "friday"
+	;;
+	7)
+	echo "saturday"
+esac
