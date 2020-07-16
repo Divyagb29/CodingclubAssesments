9ec35c5fa97aec35e5011b95eff0bf10d58f4b1f
Divyagb29 <divya2229mgb@gmail.com>
2020-07-16 19:16:48 +0530

    calculate daily employee wage

---
+++ EmployeeWageUC2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EmployeeWage;
+
+public class EmployeeWageUC2 {
+public static void main(String ag[])
+{
+    int isfulltime  = 1;
+    int emprateperhr =20;
+    int emphrs=0;
+    int empwage=0;
+    double empCheck = Math.floor(Math.random()*10)%2;
+    if(empCheck == isfulltime)
+    {
+        emphrs=8;
+    }
+    else
+    {
+        emphrs=0;
+    }
+    empwage = emphrs*emprateperhr;
+    System.out.println("Emp Wage : "+empwage);
+}
+}
